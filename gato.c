c31b480f9762aeb83aeae63e829e74f3e4f54ddd
Andrey Loaiza <andreyloaizah@gmail.com>
2023-12-10 15:00:53 -0600

    second commit

---
+++ gato.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+
+void resetBoard();
+void printBoard();
+int checkFreeSpaces();
+void playersMove();
+void computerMove();
+void printWinner(char);
+
+int main(){
+    return 0;
+}
